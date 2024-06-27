a202dc22e1294315d75c033783fad9049dc99345
rballinger <rballinger@topcon.com>
2024-06-27 15:20:54 +1000

    ..

--- Encoder.go
+++ Encoder.go
@@ -1,7 +1,6 @@
 package avro
 
 import (
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
@@ -55,55 +54,6 @@ func (encoder *Encoder) Encode(v interface{}) (err error) {
 	default:
 		return fmt.Errorf("type %s not implemented", encoder.schema.GetType())
 	}
-}
-
-func WriteBoolean(writer io.Writer, value bool) (err error) {
-	encoded := byte(0)
-	if value {
-		encoded = 1
-	}
-	_, err = writer.Write([]byte{encoded})
-	if err != nil {
-		return
-	}
-	return
-}
-
-func WriteInt(writer io.Writer, value int32) (err error) {
-	encodedValue := make([]byte, binary.MaxVarintLen32)
-	n := binary.PutVarint(encodedValue, int64(value))
-	_, err = writer.Write(encodedValue[:n])
-	if err != nil {
-		return
-	}
-	return
-}
-
-func WriteLong(writer io.Writer, value int64) (err error) {
-	encodedValue := make([]byte, binary.MaxVarintLen64)
-	n := binary.PutVarint(encodedValue, value)
-	_, err = writer.Write(encodedValue[:n])
-	if err != nil {
-		return
-	}
-	return
-}
-
-func WriteBytes(writer io.Writer, value []byte) (err error) {
-	err = WriteLong(writer, int64(len(value)))
-	if err != nil {
-		return
-	}
-	_, err = writer.Write(value)
-	if err != nil {
-		return
-	}
-	return
-}
-
-func WriteString(writer io.Writer, value string) (err error) {
-	err = WriteBytes(writer, []byte(value))
-	return
 }
 
 func (encoder *Encoder) writeRecord(v interface{}) (err error) {
--- Encoder_test.go
+++ Encoder_test.go
@@ -187,3 +187,174 @@ func TestEncode(t *testing.T) {
 		})
 	})
 }
+
+func BenchmarkEncodeBoolean(b *testing.B) {
+	schema := avroschema.AvroTypeBoolean
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		value := i%2 == 0
+		encoder.Encode(value)
+	}
+}
+
+func BenchmarkEncodeInt(b *testing.B) {
+	schema := avroschema.AvroTypeInt
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode(int32(i))
+	}
+}
+
+func BenchmarkEncodeLong(b *testing.B) {
+	schema := avroschema.AvroTypeLong
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode(int64(i))
+	}
+}
+
+func BenchmarkEncodeBytes(b *testing.B) {
+	schema := avroschema.AvroTypeBytes
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode([]byte{0x66, 0x6f, 0x6f})
+	}
+}
+
+func BenchmarkEncodeString(b *testing.B) {
+	schema := avroschema.AvroTypeString
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode("foo")
+	}
+}
+
+func BenchmarkEncodeRecord(b *testing.B) {
+	schema := &avroschema.Record{
+		SchemaBase: avroschema.SchemaBase{
+			Type: avroschema.AvroTypeRecord,
+		},
+		NamedType: avroschema.NamedType{
+			Name: "SimpleRecord",
+		},
+		Fields: []*avroschema.RecordField{
+			{
+				Name: "name",
+				Type: avroschema.AvroTypeString,
+			},
+			{
+				Name: "age",
+				Type: avroschema.AvroTypeInt,
+			},
+		},
+	}
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	type SimpleRecord struct {
+		Name string `avro:"name"`
+		Age  int32  `avro:"age"`
+	}
+	value := SimpleRecord{
+		Name: "foo",
+		Age:  42,
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode(value)
+	}
+}
+
+func BenchmarkEncodeEnum(b *testing.B) {
+	schema := &avroschema.Enum{
+		SchemaBase: avroschema.SchemaBase{
+			Type: avroschema.AvroTypeEnum,
+		},
+		NamedType: avroschema.NamedType{
+			Name: "SimpleEnum",
+		},
+		Symbols: []string{"A", "B", "C"},
+	}
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	options := []string{"A", "B", "C"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode(options[i%3])
+	}
+}
+
+func BenchmarkEncodeArray(b *testing.B) {
+	schema := &avroschema.Array{
+		SchemaBase: avroschema.SchemaBase{
+			Type: avroschema.AvroTypeArray,
+		},
+		Items: avroschema.AvroTypeInt,
+	}
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	value := []int32{42, 43}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode(value)
+	}
+}
+
+func BenchmarkEncodeMap(b *testing.B) {
+	schema := &avroschema.Map{
+		SchemaBase: avroschema.SchemaBase{
+			Type: avroschema.AvroTypeMap,
+		},
+		Values: avroschema.AvroTypeInt,
+	}
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	value := map[string]int32{"foo": 42, "bar": 43}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode(value)
+	}
+}
+
+func BenchmarkEncodeFixed(b *testing.B) {
+	schema := &avroschema.Fixed{
+		SchemaBase: avroschema.SchemaBase{
+			Type: avroschema.AvroTypeFixed,
+		},
+		NamedType: avroschema.NamedType{
+			Name: "SimpleFixed",
+		},
+		Size: 3,
+	}
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	value := []byte{0x66, 0x6f, 0x6f}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode(value)
+	}
+}
+
+func BenchmarkEncodeUnion(b *testing.B) {
+	schema := avroschema.Union{
+		avroschema.AvroTypeNull,
+		avroschema.AvroTypeInt,
+		avroschema.AvroTypeString,
+	}
+	var buffer bytes.Buffer
+	encoder := avro.NewEncoder(&buffer, schema)
+	options := []interface{}{nil, int32(42), "foo"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encoder.Encode(options[i%3])
+	}
+}
